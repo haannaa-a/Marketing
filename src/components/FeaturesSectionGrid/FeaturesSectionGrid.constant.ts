8263ed4a848a62b5fc3e3b5be9866bb001510156
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-09-24 18:51:57 +0200

    add features card grid

---
+++ src/components/FeaturesSectionGrid/FeaturesSectionGrid.constant.ts
@@ -0,0 +1,45 @@
+export const FeaturesSectionGridConstant = {
+  header: {
+    title: 'Easy access to top quality images',
+    subTitle: 'Premium abstract images',
+    text: 'In a world where storytelling constantly evolves, we lead with groundbreaking images designed for your presentation excellence.',
+  },
+  grid: [
+    {
+      id: 1,
+      title: 'Infinite Download',
+      text: "Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.",
+      dataBefore: 'assets/img/svg/thumbnail.svg',
+    },
+    {
+      id: 2,
+      title: 'Purely Handcrafted',
+      text: 'No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.',
+      dataBefore: 'assets/img/svg/brush.svg',
+    },
+    {
+      id: 3,
+      title: 'All Are Under licensed',
+      text: 'The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).',
+      dataBefore: 'assets/img/svg/copyright.svg',
+    },
+    {
+      id: 4,
+      title: 'Cancel Anytime',
+      text: "Subscribe at your own pace, and cancel when you feel it's enough.",
+      dataBefore: 'assets/img/svg/refund.svg',
+    },
+    {
+      id: 5,
+      title: 'Empowering For Team',
+      text: 'We support multiple seats at once, requiring only a single payment.',
+      dataBefore: 'assets/img/svg/team-line.svg',
+    },
+    {
+      id: 6,
+      title: 'No Limitations',
+      text: 'Use as many as you want, from Dribbble presentations to PowerPoint presentations.',
+      dataBefore: 'assets/img/svg/refresh.svg',
+    },
+  ],
+};
