8f43cd2189ef8799c726b46efc8ecdbadddb2d55
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-09-25 19:59:26 +0200

    add pricingSection

---
+++ src/components/PricingSectionSingle/PricingSectionSingle.constant.ts
@@ -0,0 +1,39 @@
+export const PricingSectionSingleConstant = {
+  header: {
+    subTitle: 'One time purchase',
+    title: 'Pay as you need',
+    text: 'We offer one-time purchases with credits, for you to use as needed. Always active.',
+  },
+  content: {
+    title: 'Unlock creativity once, enjoy forever',
+    list: [
+      {
+        id: 1,
+        text: '128 available credits for all images',
+        dataBefore: 'assets/img/svg/check.svg',
+      },
+      {
+        id: 2,
+        text: 'Up to 3 users',
+        dataBefore: 'assets/img/svg/check.svg',
+      },
+      {
+        id: 3,
+        text: '24 hour response time',
+        dataBefore: 'assets/img/svg/check.svg',
+      },
+      {
+        id: 4,
+        text: 'Advanced analytics',
+        dataBefore: 'assets/img/svg/check.svg',
+      },
+    ],
+  },
+  price: {
+    label: 'Popular',
+    price: '$699',
+    currencyType: 'Prices in USD',
+    text: 'Pay once, use it forever. No strings attached.',
+    button: 'Buy now',
+  },
+};
