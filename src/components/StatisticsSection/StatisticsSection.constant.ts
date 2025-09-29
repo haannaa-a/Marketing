d742a6bc926fe81b4db0b21f7c84f6bbf9a91d32
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-09-29 18:14:42 +0200

    add StatisticsSection

---
+++ src/components/StatisticsSection/StatisticsSection.constant.ts
@@ -0,0 +1,31 @@
+export const StatisticsSectionConstant = {
+  header: {
+    subTitle: 'Statistics',
+    title: 'More than premium abstract imagery',
+    text: 'Our platform is more than just as a service to us – it is a catalyst for enriching lives through premium abstract imagery.',
+  },
+  content: {
+    img: {
+      src: 'assets/img/abstract.jpg',
+      alt: 'abstract image',
+    },
+    title: 'Our mission, in numbers',
+    list: [
+      {
+        id: 1,
+        title: '25,664,890',
+        text: 'Downloads',
+      },
+      {
+        id: 2,
+        title: '17,219',
+        text: 'Paid users',
+      },
+      {
+        id: 3,
+        title: '190,654,321',
+        text: 'Images in library',
+      },
+    ],
+  },
+};
